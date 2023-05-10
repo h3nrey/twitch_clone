4ec6d217dbb990ccc84928ee933e12e54e3cc3e7
h3nrey <pedro.novaes.education@gmail.com>
2023-05-10 20:46:57 -0300

    fixing sections and removing section from index.tsx and moving to his own component

---
+++ src/Components/HomeSection.tsx
@@ -0,0 +1,89 @@
+import { section1Data } from "@/pages/api/homeData";
+import LiveThumbnail from "./LiveThumbnail";
+import { CaretDown } from "phosphor-react";
+import { useState } from "react";
+
+
+interface SectionProps{
+  sidebarHidden: boolean;
+  sectionTitle: string;
+}
+export default function HomeSection({sidebarHidden, sectionTitle}: SectionProps) {
+    const [isSectionOpen, setIsSectionOpen] = useState(false);
+    return(
+        <section className='mt-8 pb-2 flex flex-col gap-2 w-full'>
+          <h3 className='font-bold'>{sectionTitle}</h3>
+
+          {/* lives container */}
+          <div className='flex flex-col gap-y-4'>
+
+            {/* First row */}
+            <div className={`grid ${sidebarHidden ? 'grid-cols-4' : 'grid-cols-3'} gap-2`}>
+            {section1Data.map((live, index) => index < 4 && (
+              <>
+                {
+                  !sidebarHidden && index == 3 ? (
+                    <></>
+                  ) :
+                  <LiveThumbnail 
+                    channelName = {live.channelName}
+                    channelSubject= {live.channelSubject}
+                    channelColor= {live.channelColor}
+                    liveTitle= {live.liveTitle}
+                    liveTags={live.liveTags}
+                    liveViewers= {live.liveViewers}
+                    onLive={live.onLive}
+                    thumb={live.thumb}
+                    channelAvatar={live.channelAvatar}
+                  />
+                }
+              </>
+            ))}
+            </div>
+
+            {/* Second row */}
+            {isSectionOpen && (
+              <div className='flex gap-2'>
+              {section1Data.map((live, index) => index >= 4 && (
+                <>
+                {
+                  !sidebarHidden && index == 6 ? (
+                    <></>
+                  ) :
+                  <LiveThumbnail 
+                    channelName = {live.channelName}
+                    channelSubject= {live.channelSubject}
+                    channelColor= {live.channelColor}
+                    liveTitle= {live.liveTitle}
+                    liveTags={live.liveTags}
+                    liveViewers= {live.liveViewers}
+                    onLive={live.onLive}
+                    thumb={live.thumb}
+                    channelAvatar={live.channelAvatar}
+                  />
+                }
+              </>
+              ))}
+              </div>
+            )}
+
+            {/* section toogle button */}
+            {!isSectionOpen && (
+              <div className='relative w-full justify-center items-center flex'>
+              <hr  className='absolute top-1/2 -translate-y-1/2 text-lightgray w-full z-0'/>
+              <div className='bg-bg px-2 z-[1]'>
+                <button 
+                  onClick={() => setIsSectionOpen(true)}
+                  className="z-1 flex gap-2 items-center text-principal rounded hover:bg-gray hover:text-lightgray font-bold transition p-2">
+                Show more 
+                <span><CaretDown weight='bold'/></span></button>
+              </div>
+              
+            </div>
+            )}
+            
+          </div>
+          
+        </section>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,11 @@ import { useState } from 'react'
 import LiveThumbnail from '@/Components/LiveThumbnail'
 import { section1Data } from './api/homeData'
 import { CaretDown } from 'phosphor-react'
+import HomeSection from '@/Components/HomeSection'
 
 const inter = Inter({ subsets: ['latin'] })
 export default function App() {
   const [sidebarHidden, setSideBarHidden] = useState(false);
-  const [sectionExtraLivesHidden, setSectionExtraLivesHidden] = useState(false);
   function HandleSideBar(state: boolean) {
     setSideBarHidden(state);
   }
@@ -24,57 +24,10 @@ export default function App() {
       <main className="flex flex-col px-[1rem]" style={style}>
         <SideBar onStateChange={HandleSideBar}/>
         <Carousel />
-        <section className='mt-8 pb-2 flex flex-col gap-2 w-full'>
-          <h3 className='font-bold'>Lives that we think you'll love :)</h3>
-          <div className='flex flex-col gap-y-4'>
-            <div className={`grid ${sidebarHidden ? 'grid-cols-4' : 'grid-cols-3'} gap-2`}>
-            {section1Data.map((live, index) => index < 4 && (
-              !sidebarHidden && index != 3 && 
-              <LiveThumbnail 
-              channelName = {live.channelName}
-              channelSubject= {live.channelSubject}
-              channelColor= {live.channelColor}
-              liveTitle= {live.liveTitle}
-              liveTags={live.liveTags}
-              liveViewers= {live.liveViewers}
-              onLive={live.onLive}
-              thumb={live.thumb}
-              channelAvatar={live.channelAvatar}/>
-            ))}
-            </div>
-            {sectionExtraLivesHidden && (
-              <div className='flex gap-2'>
-              {section1Data.map((live, index) => index >= 4 && (
-                <LiveThumbnail 
-                channelName = {live.channelName}
-                channelSubject= {live.channelSubject}
-                channelColor= {live.channelColor}
-                liveTitle= {live.liveTitle}
-                liveTags={live.liveTags}
-                liveViewers= {live.liveViewers}
-                onLive={live.onLive}
-                thumb={live.thumb}
-                channelAvatar={live.channelAvatar}/>
-              ))}
-              </div>
-            )}
-            {!sectionExtraLivesHidden && (
-              <div className='relative w-full justify-center items-center flex'>
-              <hr  className='absolute top-1/2 -translate-y-1/2 text-lightgray w-full z-0'/>
-              <div className='bg-bg px-2 z-[1]'>
-                <button 
-                  onClick={() => setSectionExtraLivesHidden(true)}
-                  className="z-1 flex gap-2 items-center text-principal rounded hover:bg-gray hover:text-lightgray font-bold transition p-2">
-                Show more 
-                <span><CaretDown weight='bold'/></span></button>
-              </div>
-              
-            </div>
-            )}
-            
-          </div>
-          
-        </section>
+        <HomeSection 
+          sidebarHidden={sidebarHidden} 
+          sectionTitle="Lives that we think you'll like"
+        />
       </main>
     </div>
   )
