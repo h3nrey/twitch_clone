df02537d4b2aea57fb6a4e7dee97ed87a2b6c8be
h3nrey <pedro.novaes.education@gmail.com>
2023-06-12 14:52:16 -0300

    Add custom video player, add play, volume and fullscreen buttons and volume range

---
+++ Colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+    bg: '#0E0E10',
+    gray: '#2F2F35',
+    thingray: '#67676B',
+    lightgray: '#DEDEE3',
+    text: '#E0E0E1',
+    darkgray: '#18181B',
+    principal: '#8E31E2',
+    red: '#EB0400',
+}
