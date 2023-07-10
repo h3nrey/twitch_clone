1b55a5a2bce2d076b980ea61aeb0ab063009379f
h3nrey <pedro.novaes.education@gmail.com>
2023-07-10 00:24:59 -0300

    solving pages withour route

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,27 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from '@/Components/Header'
-import SideBar from '@/Components/Sidebar'
-import Carousel from '@/Components/Carousel'
-import { useState } from 'react'
-import LiveThumbnail from '@/Components/LiveThumbnail'
-import { section1Data } from './api/homeData'
-import { CaretDown } from 'phosphor-react'
-import HomeSection from '@/Components/HomeSection'
-
-const inter = Inter({ subsets: ['latin'] })
 export default function App() {
-  const [sidebarHidden, setSideBarHidden] = useState(false);
-  function HandleSideBar(state: boolean) {
-    setSideBarHidden(state);
-  }
 
-  const style = {
-    marginLeft: sidebarHidden ? "3rem" : "12.5rem"
-  }
   return (
     <div className='text-text pt-[3rem]'>
-      peixonauta
+      Página não criada
     </div>
   )
 }
